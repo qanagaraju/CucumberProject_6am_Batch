a51072d1c59c21175907beb7e9f3e8dc8fb6e335
qanagaraju <nagarazu@gmail.com>
2022-01-21 06:54:55 +0530

    second commit

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -8,9 +8,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 //@RunWith(Cucumber.class)
 @io.cucumber.testng.CucumberOptions
 (
-		features = {"D:\\Selenium_Examples_New\\com.Cucucmber_Examples\\Features\\LoginWindow.feature"},
+		features = {"D:\\Selenium_Examples_New\\com.Cucucmber_Examples\\Features\\LoginWindow.feature",
+				"D:\\Selenium_Examples_New\\com.Cucucmber_Examples\\Features\\AmazonSearch.feature"},
 		glue= {"stepDefinitions"},
-		plugin = {"pretty"}
+		
+		//plugin = { "pretty", "html:target/cucumber-reports" }
+				//plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" }
+				plugin = { "pretty", "junit:target/cucumber-reports/Cucumber.xml","json:target/cucumber-reports/Cucumber.json"}
+				
 		
 		
 		)
